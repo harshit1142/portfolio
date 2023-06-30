f269fc9ca872ef9d9cf6e60e2573a435ffa79b20
harshit1142 <harshitpandey98765@gmail.com>
2023-06-30 12:53:53 +0530

    "Navbar done"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./Components/Navbar";
+import Home from "./Screens/Home";
 
 
 function App() {
   return (
-   <h1>HP</h1>
+      <Home />
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='container-fluid nav-bar ' >
+          <div className='d-flex align-content center align-items-center h-100 pt-2'>
+            <div className='logo fs-1 d-flex col-lg-4 col-md-3 col-sm-2 text-white'data-toggle="tooltip" data-placement="bottom" title="HARSHIT PANDEY">HARSHIT.</div>
+            <div className='navlinks '>
+                <ul className='col fs-5  d-flex flex-wrap align-content-center text-warming'>
+                    <li><a href="#home" className='btn btn-outline-light' >HOME</a></li>
+                    <li><a href="#about" className='btn btn-outline-light'>ABOUT</a></li>
+                    <li><a href="#skill" className='btn btn-outline-light'>SKILL</a></li>
+                    <li><a href="#education" className='btn btn-outline-light'>EDUCATION</a></li>
+                    <li><a href="#project" className='btn btn-outline-light'>PROJECTS</a></li>
+                    <li><a href="#contact" className='btn btn-outline-light'>CONTACT</a></li>
+                </ul>
+            </div>
+          </div>
+    </div>
+  )
+}
