690f985d7863f3b615e5bbb9fe63b3dde5345dcf
harshit1142 <harshitpandey98765@gmail.com>
2024-03-27 13:55:54 +0530

    Fix:Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,8 +4,9 @@ export default function Navbar() {
 
 
   const [colorChange, setColorchange] = useState(false);
+  const [toggle, setToggle] = useState(true);
   const changeNavbarColor = () => {
-    if (window.scrollY >= 80) {
+    if (window.scrollY >= 80 || toggle==true) {
       setColorchange(true);
     } else {
       setColorchange(false);
@@ -13,13 +14,20 @@ export default function Navbar() {
   };
   window.addEventListener("scroll", changeNavbarColor);
 
-
+ function handleToggle(){
+  if(toggle==true){
+    setToggle(false);
+  }else{
+    setToggle(true);
+  }
+  changeNavbarColor();
+ }
   return (
     <div>
       <nav className={colorChange ? "navbar fixed-top navbar-expand-lg bg-dark  " :"navbar fixed-top navbar-expand-lg bg-transparent  "}>
   <div className="container-fluid ">
     <div className='logo fs-1 d-flex col-lg-4 col-md-4 col-sm-4 text-white'data-toggle="tooltip" data-placement="bottom" title="HARSHIT PANDEY" id="logo"> HarshitPandey.in</div>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" onClick={()=>handleToggle()} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon bg-white"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
